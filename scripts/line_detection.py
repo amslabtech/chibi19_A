1f65bae418ccdc2737e09c8fe706eb5229167c00
sugiura <ee63052@meiji.ac.jp>
2019-03-21 12:24:19 +0900

    line detection

--- scripts/line_detection.py
+++ scripts/line_detection.py
@@ -10,14 +10,10 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
-
-#fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
-#video = cv2.VideoWriter('video.mp4', fourcc, 5.0, (640, 480))
-
 class image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("image",Image)
+    self.detection_pub = rospy.Publisher("line_detection",String)
 
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
@@ -33,47 +29,34 @@ class image_converter:
     for i in range(2):
         cv_image2 = cv2.GaussianBlur(cv_image2,(3,3),0)
         cv_image2 = cv2.medianBlur(cv_image2,3)
+
     ret, thresh = cv2.threshold(cv_image2,205,255,0)
-   # th2 = cv2.adaptiveThreshold(cv_image2,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,20)
     image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     
     cv_image = cv2.drawContours(cv_image,contours,-1,(0,255,0),3)
+
     rects = list(map(cv2.minAreaRect, contours))
     boxs = list(map(cv2.boxPoints, rects))
     boxs = list(map(np.int0, boxs))
     areas = list(map(cv2.contourArea, boxs))
     cv_image = cv2.drawContours(cv_image,boxs,-1,(255,0,0),2)
- #   print(areas)
     for i in range(len(contours)):
-#        rect = cv2.minAreaRect(contours[i])
         w, h = rects[i][1]
-#        box = cv2.boxPoints(rect)
-#        box = np.int0(box)
-#        area = cv2.contourArea(box)
         if areas[i] != 0 and w != 0 and h != 0:
             if ((float(w) / h) < 0.2) and areas[i] > 10000:
-              cv_image = cv2.drawContours(cv_image,[boxs[i]],0,(0,0,255),2)
-              #print(areas[i])
-              print(float(w) / h)
-
- #   cv_image = cv2.drawContours(cv_image,boxs,-1,(0,0,255),2)
-
-
-   # cv_image = cv2.drawContours(cv_image,contours,-1,(0,255,0),3)
-   # cv_image3 = cv2.Canny(gray_image, 50, 150.0)
-   # cv_image3 = cv2.Laplacian(cv_image2, cv2.CV_32F)
-   #cv2.imshow("Image window", cv_image2)
-
- #   video.write(cv_image)
+                cv_image = cv2.drawContours(cv_image,[boxs[i]],0,(0,0,255),2)
+                #print(areas[i])
+                #print(float(w) / h)
+                
+                try:
+                    self.detection_pub.publish("aaa")
+                except CvBridgeError as e:
+                    print(e)
 
     cv2.imshow("Image window1", thresh)
     cv2.imshow("Image window2", cv_image)
     cv2.waitKey(3)
 
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
 
 def main(args):
   ic = image_converter()
